fca707a3774895f7f78445909ba637c55a327f44
TAO-YI LEE <tylee@ieee.org>
2013-05-05 14:50:42 +0800

    DUALP

---
+++ deinit_hfssapi.m
@@ -0,0 +1,13 @@
+pathdir = pwd;
+path = '/boundary/';
+rmpath([pathdir path]);
+disp(['Path "' pathdir path '" removed'])
+path = '/3dmodeler/';
+rmpath([pathdir path]);
+disp(['Path "' pathdir path '" removed'])
+path = '/general/';
+rmpath([pathdir path]);
+disp(['Path "' pathdir path '" removed'])
+path = '/analysis/';
+rmpath([pathdir path]);
+disp(['Path "' pathdir path '" removed'])
---
+++ init_hfssapi.m
@@ -0,0 +1,26 @@
+prefix = '../';
+pathdir = pwd;
+path = [prefix '/sbox/'];
+addpath([pathdir path]);
+disp(['Path "' pathdir path '" appended'])
+path = [prefix '/boundary/'];
+addpath([pathdir path]);
+disp(['Path "' pathdir path '" appended'])
+path = [prefix '/3dmodeler/'];
+addpath([pathdir path]);
+disp(['Path "' pathdir path '" appended'])
+path = [prefix '/analysis/'];
+addpath([pathdir path]);
+disp(['Path "' pathdir path '" appended'])
+path = [prefix '/general/'];
+addpath([pathdir path]);
+disp(['Path "' pathdir path '" appended'])
+
+% Temporary Files. These files can be deleted after the optimization
+% is complete. We have to specify the complete path for all of them.
+tmpPrjFile = 'E:\temp\tmpDipole.hfss';
+tmpDataFile = 'E:\temp\tmpData.m';
+
+% HFSS Executable Path.
+%"C:\Program Files (x86)\Ansoft\HFSS13.0\hfss.exe"
+hfssExePath = '"C:\Program Files\Ansoft\HFSS14.0\Win64\hfss.exe"';
